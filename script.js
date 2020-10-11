d02cf28ab58ed0d770345acd682e52ec8be25cee
JC <sanjothebay@gmail.com>
2020-10-11 18:20:21 -0500

    added questions

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ the correct selections included
 var generateBtn = document.querySelector("#generate");
 
 var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
-
+var numbers = confirm("Do you want numbers in your password?");
+var lowerCases = confirm("Do you want lowercases in your password?");
+var upperCases = confirm("Do you want uppercases in your password?");
 
 // Write password to the #password input
 function writePassword() {
