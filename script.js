bba86ca885d36bc0aebf31ec89f99c3554733d40
JC <sanjothebay@gmail.com>
2020-10-10 19:28:21 -0500

    new save

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+
+/*
+when clicked on generate a password Enduser is asked:
+1: length of the password 
+(length of at least 8 characters and no more than 128 characters)
+
+2: character types to include in the password
+(lowercase, uppercase, numeric, and/or special characters)
+
+3:After given answers the input should be validated 
+and give at least one character type
+
+4:After Validtaion then a password is given with 
+the correct selections included
+
+5:Displayed for the end user to see 
+*/
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+prompt("How many characters would you like your password to be? (8-128)")
+confirm("Would you like to include lowercase letters? ")
+confirm("Would you like to include uppercase letters?")
+confirm("Would you like to include numbers?")
+confirm("Would you like to include special characters?")
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +34,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
