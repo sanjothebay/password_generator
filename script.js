bd3c2d23640dce3991f043c60c795798aade7c7f
JC <sanjothebay@gmail.com>
2020-10-10 21:05:46 -0500

    made better changes

--- script.js
+++ script.js
@@ -14,19 +14,39 @@ and give at least one character type
 the correct selections included
 
 5:Displayed for the end user to see 
+
+for (var i = 0; i < 10; i++)
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+UsersAnswers = UsersAnswers.toLowerCase();
+UsersAnswers = UsersAnswers.toLowerCase();
 */
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var UsersAnswers = 0;
+var charactersChoice = 0;
+var lowercaseLettersChoice = 0;
+var uppercaseLettersChoice = 0;
+var numberChoice = 0;
+var specialCharactersChoice = 0;
+
+var questionsAsked = [
+  { question: "How many characters would you like your password to be? (8-128)", answers: "" },
+  { question: "Would you like to include lowercase letters? ", answers: "t" || "f" },
+  { question: "Would you like to include uppercase letters?", answers: "t" || "f" },
+  { question: "Would you like to include numbers?", answers: "t" || "f" },
+  { question: "Would you like to include special characters?", answers: "t" || "f" }
+];
+for (var i = 0; i < questionsAsked.length; i++) {
+  var answers = confirm(questionsAsked[i].q);
+}
 
 
-prompt("How many characters would you like your password to be? (8-128)")
-confirm("Would you like to include lowercase letters? ")
-confirm("Would you like to include uppercase letters?")
-confirm("Would you like to include numbers?")
-confirm("Would you like to include special characters?")
+var charactersChoice = numberChoice[Math.floor(Math.random() * numberChoice.length)];
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -36,6 +56,16 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
+
+
+if (charactersChoice < 8 ) {
+  alert("Please pick a number higher than 8")
+  
+} 
+else confirm(lowercaseLettersChoice) {
+  
+}
 
 
 
